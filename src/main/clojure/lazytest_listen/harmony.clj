c2816f50fedd86f0dc9f4d28cf59e5e9e514e0c1
bgruber <github.com@brian.iheardata.com>
2010-11-11 03:43:27 -0500

    fix harmony display so it works with new lazytest

--- src/main/clojure/lazytest_listen/harmony.clj
+++ src/main/clojure/lazytest_listen/harmony.clj
@@ -1,6 +1,6 @@
 (ns lazytest-listen.harmony
-  (:require [com.stuartsierra.lazytest [watch :as lw]])
-  (:use [com.iheardata.lazytest-listen]
+  (:require [lazytest.watch :as lw])
+  (:use [lazytest-listen]
         [clojure.set :only (difference)])
   (:import [javax.sound.midi MidiSystem]))
 
@@ -15,10 +15,10 @@
 (def velocity 127)
 
 (defn midi-transition
-  "Construct a list of MIDI messages that turns off all notes in
-   old-pitches that are not in new-pitches, and turns on any notes in
-   new-pitches that are not in old-pitches. old-pitches and
-   new-pitches are sets of integers representing MIDI pitches."
+  "Turns off all notes in old-pitches that are not in new-pitches, and
+   turns on any notes in new-pitches that are not in
+   old-pitches. old-pitches and new-pitches are sets of integers
+   representing MIDI pitches."
   [old-pitches new-pitches note-on-velocity channel]
   (doseq [pitch (difference old-pitches new-pitches)]
     (.noteOff channel pitch))
@@ -29,26 +29,20 @@
   (midi-transition (chords (:ci state)) (chords ci) velocity (get-channel (:synth state) 0))
   (assoc state :ci ci))
 
-(defn stop [state]
-  (stop-synth (:synth state))
-  (send (:watch-dir-agent state) lw/stop)
-  state)
-
-(defn listen-spec [d & options]
-  (let [synth (MidiSystem/getSynthesizer)
-        agnt (agent {})
-        watch-dir-agent (apply lw/watch-spec
-                               d
-                               :reporter #(send agnt
-                                                change-chord
-                                                (Math/round (* 4.0 (fraction-failed %))))
-                               options)]
+(defn start [dirs & options]
+  (let [synth (MidiSystem/getSynthesizer)]
     (.open synth)
     (let [chan (get-channel synth 0)]
       (.programChange chan prog)
       (.controlChange chan 7 init-vol)
-      (doseq [p (chords 0)] (.noteOn chan p velocity)))    
-    (send agnt #(assoc %
-                  :synth synth
-                  :watch-dir-agent watch-dir-agent
-                  :ci 0))))
+      (doseq [p (chords 0)] (.noteOn chan p velocity)))
+    (let [synth-state (agent {:synth synth
+                              :ci 0})
+          reporter    #(send synth-state
+                             change-chord
+                             (Math/round (* 4.0 (fraction-failed %))))]
+      (apply lw/start
+             dirs
+             :report-fn reporter
+             options))))
+
