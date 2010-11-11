4f40d7658a502cbfd9397c46347d7ed3312c7b62
bgruber <github.com@brian.iheardata.com>
2010-11-11 03:52:48 -0500

    fix volume display to work with new lazytest

--- src/main/clojure/lazytest_listen/volume.clj
+++ src/main/clojure/lazytest_listen/volume.clj
@@ -1,6 +1,6 @@
 (ns lazytest-listen.volume
   (:use [lazytest-listen])
-  (:require [com.stuartsierra.lazytest [watch :as lw]])
+  (:require [lazytest.watch :as lw])
   (:import [javax.sound.midi MidiSystem]))
 
 (def prog 78) ;; GM whistle
@@ -19,38 +19,22 @@
         (when (= 0 old-vol)
           (.noteOn channel pitch 127))))))
 
-(defn stop [state]
-  (let [{:keys [synth watch-dir-agent]} state]
-    (stop-synth synth)
-    (send watch-dir-agent lw/stop))
-  state)
-
-(defn reset [state]
-  (let [{:keys [synth watch-dir-agent]} state]
-    (doto (get-channel synth 0)
-      (.allNotesOff)
-      (change-volume 0 init-volume))
-    (send watch-dir-agent lw/reset))
-  state)
-
 (defn volume [state vol]
   (change-volume (get-channel (:synth state) 0) (:volume state) vol)
   (assoc state :volume vol))
 
-(defn listen-spec [d & options]
-  (let [synth (MidiSystem/getSynthesizer)
-        agnt (agent {})
-        watch-dir-agent (apply lw/watch-spec
-                               d
-                               :reporter #(send agnt
-                                                volume
-                                                (int (* 127 (fraction-failed %))))
-                               options)]
+(defn start [dirs & options]
+  (let [synth (MidiSystem/getSynthesizer)]
     (.open synth)
     (doto (get-channel synth 0)
       (.programChange prog)
       (change-volume 0 init-volume))
-    (send agnt #(assoc %
-                  :synth synth
-                  :watch-dir-agent watch-dir-agent
-                  :volume init-volume))))
+    (let [synth-state (agent {:synth  synth
+                              :volume init-volume})
+          reporter    #(send synth-state
+                             volume
+                             (int (* 127 (fraction-failed %))))]
+      (apply lw/start
+             dirs
+             :report-fn reporter
+             options))))
