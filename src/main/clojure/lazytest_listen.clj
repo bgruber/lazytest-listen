002939ac6f45cdbc10df47aa80404a369d3f4454
bgruber <github.com@brian.iheardata.com>
2010-11-26 13:29:56 -0500

    fix reflection problem on windows

--- src/main/clojure/lazytest_listen.clj
+++ src/main/clojure/lazytest_listen.clj
@@ -1,8 +1,10 @@
 (ns lazytest-listen
-  (:require [lazytest.results :as lr]))
+  (:require [lazytest.results :as lr])
+  (:import (javax.sound.midi Synthesizer)))
 
 (defn get-channel [synth channel]
-  (aget (.getChannels synth) channel))
+  ;; must type hint Synthesizer to avoid reflection error on Windows.
+  (aget (.getChannels ^Synthesizer synth) channel))
 
 (defn stop-synth [synth]
   (.allNotesOff (get-channel synth 0))
