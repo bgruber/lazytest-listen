17c09c44d7eb1790e6a68959255b62e46ac779e8
bgruber <github.com@brian.iheardata.com>
2010-11-26 19:50:24 -0500

    switch to leiningen, cause i couldn't deal with my build file being as long as the source.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject com.iheardata/lazytest-listen "1.1.2-SNAPSHOT"
+  :description "Auditory displays for lazytest"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [com.stuartsierra/lazytest "1.1.2"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]]
+  :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"})
