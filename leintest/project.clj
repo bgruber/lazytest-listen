214b81414ee9712801c52afce8dd633a21344f5f
bgruber <github.com@brian.iheardata.com>
2010-12-06 00:56:52 -0500

    include lazytest itself in example project dependencies

--- leintest/project.clj
+++ leintest/project.clj
@@ -2,4 +2,6 @@
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[com.iheardata/lazytest-listen "1.1.2-SNAPSHOT"]])
+  :dev-dependencies [[com.stuartsierra/lazytest "1.1.2"]
+		     [com.iheardata/lazytest-listen "1.1.2-SNAPSHOT"]]
+  :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"})
